af328158a45b3cbbdd6cce44dfa478bb502341aa
richardunisul <richard.souza@animaeducacao.com.br>
2023-08-03 18:50:46 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+//const express = require('express');
+//const cors = require('cors');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//app.use(cors());
+//app.use(express.json());
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
